3fc9b4bb7a23f728f3124070ac2e9af2a9033255
Raphael Fleury <raphael.fleury9@gmail.com>
2026-04-04 17:05:54 -0300

    feat: add location schema for defining location data structure

---
+++ convex/schemas/location.schema.ts
@@ -0,0 +1,22 @@
+import { Infer, v } from "convex/values";
+
+const nominatimLocationSchema = v.object({
+  addresstype: v.string(),
+  boundingbox: v.array(v.string()),
+  class: v.string(),
+  display_name: v.string(),
+  importance: v.number(),
+  lat: v.string(),
+  licence: v.string(),
+  lon: v.string(),
+  name: v.string(),
+  osm_id: v.number(),
+  osm_type: v.string(),
+  place_id: v.number(),
+  place_rank: v.number(),
+  type: v.string(),
+});
+
+export const locationSchema = nominatimLocationSchema;
+
+export type Location = Infer<typeof locationSchema>;
